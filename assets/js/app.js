689db876da7790dccd5010bf492627b9df742356
cabrown91 <cleo.aliciabrown@gmail.com>
2016-08-19 07:35:05 -0700

    add jquery for contact button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,10 @@
 // sanity check
 console.log("app.js linked.");
+
+$(document).ready(function(){
+
+  $("#contact-button").on("click", function handleClick(event) {
+    console.log("clicked!");
+  });
+
+});
